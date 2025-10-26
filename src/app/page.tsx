986827cb34bef850f35355564cac374be9b195ad
minhduoc-tran <tmduoc.169@gmail.com>
2025-10-26 18:58:21 +0700

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
       <h1 className="text-4xl font-bold text-zinc-800 dark:text-zinc-200">
         Next.js CI/CD with Docker
       </h1>
+      <p className="mt-4 text-lg text-zinc-600 dark:text-zinc-400">
+        This is a simple example of a Next.js application with CI/CD using
+        Docker.
+      </p>
     </div>
   );
 }
